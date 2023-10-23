268e58201201c8c16814a82eebef373cf3a19e0d
Ahmed Ali Khan <ahmedalikhan652@gmail.com>
2023-10-23 11:40:07 -0400

    learned about different types of comments in Kotlin

---
+++ src/main/kotlin/Comments.kt
@@ -0,0 +1,7 @@
+fun main() {
+    // println("This is a single line comment")
+    /*
+    you can have multiple comment in multiple lines between multi-line comments.
+    println("This is a multi line comment")
+     */
+}
