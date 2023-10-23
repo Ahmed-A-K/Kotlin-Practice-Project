3531582c3364eaa0df98c9d41a63afbd79249721
Ahmed Ali Khan <ahmedalikhan652@gmail.com>
2023-10-23 11:42:46 -0400

    learned about variables

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,6 @@
+fun main() {
+    // Kotlin is static types so  we give datatype to a variable after its name
+    var name:String = "Ahmed"
+    var age:Int = 20
+    print("Name: ${name} Age: ${age}")
+}
