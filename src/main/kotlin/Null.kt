8955c802df3167ac3bde90ca4797d881e7a2b784
Ahmed Ali Khan <ahmedalikhan652@gmail.com>
2023-10-23 12:25:20 -0400

    Learned about null safety in kotlin and how to add null safety to a variable.

--- src/main/kotlin/Null.kt
+++ src/main/kotlin/Null.kt
@@ -1,6 +1,6 @@
 fun main() {
     //Kotlin handles null point exception by not allowing us to assign variables name to null.
-    //to be able to have variables to have null values we use a ? with the variable name.
+    //to be able to have variables to have null values we use a ? with the data type.
 
     var brand: String? = null;
     print(brand?.uppercase()) // we need ? with the variable name inorder to use string methods with the null safe variable.
