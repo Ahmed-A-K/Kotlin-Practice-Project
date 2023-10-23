4d08a2ede610256cb632905d44f3f25345711c78
Ahmed Ali Khan <ahmedalikhan652@gmail.com>
2023-10-23 12:01:30 -0400

    learned about the Any data type

---
+++ src/main/kotlin/Any.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var name:Any = "String"
+    var age:Any = 10
+    //The Any data type is the data type from which all other data types are derived from in Kotlin.
+    //With the Any type our variable can hold any data type.
+    print("Name: ${name} Age: ${age}")
+}
