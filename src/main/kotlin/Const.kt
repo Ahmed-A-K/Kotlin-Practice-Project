9ae0c98b8264dfd92b7db2605ea244dcb05f9072
Ahmed Ali Khan <ahmedalikhan652@gmail.com>
2023-10-23 12:13:07 -0400

    Learned about const keyword.

---
+++ src/main/kotlin/Const.kt
@@ -0,0 +1,7 @@
+const val PI = 3.14; // static final in java
+//we declare it outside the function because this value is know before execution.
+
+fun main() {
+    val name = "ahmed" // read-only --> final in java
+    var i = 19; // mutable
+}
