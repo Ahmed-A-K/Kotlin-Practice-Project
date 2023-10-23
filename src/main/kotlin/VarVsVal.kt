cb2bff86b8e88d47fe58456ba4a3224de71960ee
Ahmed Ali Khan <ahmedalikhan652@gmail.com>
2023-10-23 12:08:57 -0400

    The difference between var and val keyword.

---
+++ src/main/kotlin/VarVsVal.kt
@@ -0,0 +1,9 @@
+fun main() {
+    //var variables are mutable.
+    var name:String = "Ahmed"
+    name = "Ahmed Ali Khan"
+
+    //val variables are not mutable. They are read-only once declared and assigned.
+    val age:Int = 19;
+    // age = 20; this code gives an error indicating that age cannot be mutated or changed
+}
